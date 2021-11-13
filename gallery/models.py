1b0154a2dcf5cb55d916de6833c60e95621b494d
Karolis J <jasinevicius4@gmail.com>
2021-11-13 21:34:36 +0000

    improve static/media file handling

--- gallery/models.py
+++ gallery/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import uuid, os
 
 class ImageCategory(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
     name = models.CharField(max_length=200)
     slug = models.CharField(max_length=200)
 
@@ -14,31 +15,21 @@ class ImageCategory(models.Model):
 
 
 class Image(models.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
-    #Rename uploaded picture to the UUID
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
+    #Rename uploaded picture to the ID
     def path_and_rename(self, filename):
-        upload_to = 'gallery/static/gallery/pictures/'
+        upload_to = 'images/'
         ext = filename.split('.')[-1]
-        if self.uuid:
-            filename = '{}.{}'.format(self.uuid, ext)
-        else:
-            filename = '{}.{}'.format('fail', ext)
+        filename = f'{self.id}.{ext}'
         return os.path.join(upload_to, filename)
     
-    picture = models.ImageField(upload_to=path_and_rename, default='gallery/static/gallery/pictures/default/no-img.jpg')
+    image = models.ImageField(upload_to=path_and_rename, default='background-magic.jpg')
+    # image = models.ImageField(default='background-magic.jpg')
     title = models.CharField(max_length=200, unique=True)
     date = models.DateTimeField("Date added", default=datetime.now())
     SFW = models.BooleanField(default=True)
     category = models.ManyToManyField(ImageCategory, verbose_name="Category")
-    url = models.CharField(max_length=200, default='http://localhost:8000/id/1337')
 
-    def image_url(self):
-        return str(self.picture.name).lstrip("gallery")
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        self.url = "http://localhost:8000/id/" + str(self.pk)
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return str(self.pk) + " - " + str(self.title)
--- gallery/views.py
+++ gallery/views.py
@@ -5,8 +5,9 @@ from .models import Image, ImageCategory
 from .serializers import ImageSerializer
 from rest_framework import viewsets
 from rest_framework.parsers import JSONParser
+import uuid
 
-#TODO display order, pictures per page and display columns should be saved in cookie to not mess URL
+#TODO display order, images per page and display columns should be saved in cookie to not mess from 
 
 def api_image_list(request):
     if request.method == 'GET':
@@ -15,6 +16,7 @@ def api_image_list(request):
         return JsonResponse(serializer.data, safe=False)
     else:
         return HttpResponse('Only GET method is allowed for now')
+
 
 def api_image(request, pk):
     try:
@@ -28,37 +30,26 @@ def api_image(request, pk):
     else:
         return HttpResponse('Only GET method is allowed for now')
 
+
 def image(request, pk):
-    images = [p.pk for p in Image.objects.all()]
-    if pk in images:
+    try:
         this_image = Image.objects.get(pk=pk)
-        picture_url = this_image.image_url()
-        image_data = open("gallery" + picture_url, "rb").read()
-        if (picture_url.split(".")[-1] == "jpg") or (picture_url.split(".")[-1] == "jpeg") or (picture_url.split(".")[-1] == "jpe"):
-            response = HttpResponse(image_data, content_type="image/jpeg")
-            response['Content-Disposition'] = 'filename="' + str(this_image.uuid) + '.jpg"'
-            return response
-        elif (picture_url.split(".")[-1] == "png"):
-            response = HttpResponse(image_data, content_type="image/png")
-            response['Content-Disposition'] = 'filename="' + str(this_image.uuid) + '.png"'
-            return response
-        elif (picture_url.split(".")[-1] == "gif"):
-            response = HttpResponse(image_data, content_type="image/gif")
-            response['Content-Disposition'] = 'filename="' + str(this_image.uuid) + '.gif"'
-            return response
-        else:
-            return HttpResponse("Unknown image mimetype.")
-    else:
-        return HttpResponse("No such image")
+    except Image.DoesNotExist:
+        return HttpResponse(status=404)
+    return redirect(this_image.image.url)
+        
 
 def homepage(request):
     return render(request=request, template_name='gallery/home.html')
 
+
 def api(request):
     return render(request=request, template_name='gallery/api.html')
 
+
 def random(request):
     return HttpResponse('Random pic here')
+
 
 def gallery(request):
     order_by = request.GET.get('order')
@@ -69,9 +60,9 @@ def gallery(request):
         order_by = "newest"
     these_categories = ImageCategory.objects.all()
 
-    pictures_per_page = request.GET.get('pictures')
-    if pictures_per_page == None:
-        pictures_per_page = '25'
+    images_per_page = request.GET.get('images')
+    if images_per_page == None:
+        images_per_page = '25'
 
     images_per_width = request.GET.get('display')
     if images_per_width == "1":
@@ -85,17 +76,18 @@ def gallery(request):
     else:
         display_format = {"col_style":"col-6", "width":"w-100", "display":"2"}
     
-    display_format.update({"pictures_per_page": pictures_per_page})
+    display_format.update({"images_per_page": images_per_page})
     display_format.update({"order_by":order_by})
     display_format.update({"category":"All"})
     display_format.update({"total_images":str(len(all_images))})
 
-    paginator = Paginator(all_images, int(pictures_per_page))
+    paginator = Paginator(all_images, int(images_per_page))
 
     page = request.GET.get('page')
     these_images = paginator.get_page(page)
 
     return render(request, 'gallery/gallery.html', {"images": these_images, "categories": these_categories, "display": display_format})
+
 
 def category_slug(request, category_slug):
     category = ImageCategory.objects.filter(slug=category_slug)[0]
@@ -107,9 +99,9 @@ def category_slug(request, category_slug):
         order_by = "newest"
     these_categories = ImageCategory.objects.all()
 
-    pictures_per_page = request.GET.get('pictures')
-    if pictures_per_page == None:
-        pictures_per_page = '25'
+    images_per_page = request.GET.get('images')
+    if images_per_page == None:
+        images_per_page = '25'
 
     images_per_width = request.GET.get('display')
     if images_per_width == "1":
@@ -123,17 +115,18 @@ def category_slug(request, category_slug):
     else:
         display_format = {"col_style":"col-6", "width":"w-100", "display":"2"}
     
-    display_format.update({"pictures_per_page": pictures_per_page})
+    display_format.update({"images_per_page": images_per_page})
     display_format.update({"order_by":order_by})
     display_format.update({"category":category.slug})
     display_format.update({"total_images":str(len(category_images))})
 
-    paginator = Paginator(category_images, int(pictures_per_page))
+    paginator = Paginator(category_images, int(images_per_page))
 
     page = request.GET.get('page')
     these_images = paginator.get_page(page)
 
     return render(request, 'gallery/gallery.html', {"images": these_images, "categories": these_categories, "display": display_format})
+
 
 def about(request):
     return HttpResponse('About')
