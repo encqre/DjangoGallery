b967c884be8354c069f93b8c0e89171b4401b7c7
Karolis J <jasinevicius4@gmail.com>
2022-01-22 16:22:36 +0000

    update category endpoints to use DRF decorator and response objects for function based views

--- gallery/views.py
+++ gallery/views.py
@@ -4,8 +4,10 @@ from django.views.decorators.csrf import csrf_exempt # TEMPORARY
 from django.core.paginator import Paginator
 from .models import Image, ImageCategory
 from .serializers import ImageSerializer, CategorySerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, status, permissions
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 import uuid
 
 #TODO display order, images per page and display columns should be saved in cookie to not mess from 
@@ -32,45 +34,47 @@ def api_image(request, pk):
         return HttpResponse('Only GET method is allowed for now')
 
 
-@csrf_exempt # TODO remove later
+# Using function based view approach
+@api_view(['GET', 'POST'])
+@permission_classes((permissions.AllowAny,))
 def api_category_list(request):
     if request.method == 'GET':
         categories = ImageCategory.objects.all()
         serializer = CategorySerializer(categories, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
+    
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = CategorySerializer(data=data)
+        serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     else:
         return HttpResponse('Only GET and POST methods are allowed for now')
 
 
-@csrf_exempt # TODO remove later
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes((permissions.AllowAny,))
 def api_category(request, category_slug):
     try:
         category = ImageCategory.objects.get(slug=category_slug)
     except ImageCategory.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
         serializer = CategorySerializer(category)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = CategorySerializer(category, data=data)
+        serializer = CategorySerializer(category, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         category.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     else:
         return HttpResponse('Only GET/PUT/DELETE methods are allowed')
